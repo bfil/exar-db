9d428ebb38c94312c4ac50d1ad6e4c59f18b436f
Bruno Filippone <bruno.filippone@b-fil.com>
2016-02-25 21:32:19 +0000

    Added query tests

--- exar-core/src/query.rs
+++ exar-core/src/query.rs
@@ -69,3 +69,79 @@ impl Query {
         self.count += 1;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+
+    #[test]
+    fn test_constructors_and_modifiers() {
+        let query = Query::new(true, 100, Some(20), Some("tag".to_owned()));
+        assert_eq!(query.live, true);
+        assert_eq!(query.offset, 100);
+        assert_eq!(query.limit, Some(20));
+        assert_eq!(query.tag, Some("tag".to_owned()));
+        assert_eq!(query.is_live(), true);
+
+        let query = Query::current();
+        assert_eq!(query.live, false);
+        assert_eq!(query.offset, 0);
+        assert_eq!(query.limit, None);
+        assert_eq!(query.tag, None);
+        assert_eq!(query.is_live(), false);
+
+        let query = Query::live();
+        assert_eq!(query.live, true);
+        assert_eq!(query.offset, 0);
+        assert_eq!(query.limit, None);
+        assert_eq!(query.tag, None);
+
+        let query = query.offset(100);
+        assert_eq!(query.offset, 100);
+
+        let query = query.limit(20);
+        assert_eq!(query.limit, Some(20));
+
+        let query = query.by_tag("tag");
+        assert_eq!(query.tag, Some("tag".to_owned()));
+    }
+
+    #[test]
+    fn test_event_matching() {
+        let mut query = Query::current();
+
+        assert!(query.matches(&Event::new("data", vec!["tag1"]).with_id(1)));
+
+        query.update(1);
+
+        assert!(!query.matches(&Event::new("data", vec!["tag1"]).with_id(1)));
+
+        let mut query = Query::current().by_tag("tag1");
+
+        assert!(query.matches(&Event::new("data", vec!["tag1"]).with_id(1)));
+        assert!(!query.matches(&Event::new("data", vec!["tag2"]).with_id(1)));
+
+        query.update(1);
+
+        assert!(!query.matches(&Event::new("data", vec!["tag1"]).with_id(1)));
+    }
+
+    #[test]
+    fn test_is_active() {
+        let query = Query::current();
+
+        assert!(query.is_active());
+
+        let mut query = query.limit(3);
+
+        assert!(query.is_active());
+
+        query.update(1);
+        query.update(2);
+        query.update(3);
+
+        assert_eq!(query.position, 3);
+        assert_eq!(query.count, 3);
+        assert!(!query.is_active());
+    }
+}

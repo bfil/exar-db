99f843b4613e14a588904af573e1b4dab74629ec
Bruno Filippone <bruno.filippone@b-fil.com>
2016-02-25 22:20:48 +0000

    Added routing_strategy test

--- exar-core/src/routing_strategy.rs
+++ exar-core/src/routing_strategy.rs
@@ -45,7 +45,7 @@ impl Serialize for RoutingStrategy {
 #[cfg(feature = "serde-serialization")]
 impl Deserialize for RoutingStrategy {
     fn deserialize<D: Deserializer>(deserializer: &mut D) -> Result<Self, D::Error> {
-        deserializer.visit_string(RoutingStrategyVisitor)
+        deserializer.visit_str(RoutingStrategyVisitor)
     }
 }
 
@@ -55,8 +55,8 @@ struct RoutingStrategyVisitor;
 #[cfg(feature = "serde-serialization")]
 impl Visitor for RoutingStrategyVisitor {
     type Value = RoutingStrategy;
-    fn visit_string<E: Error>(&mut self, s: String) -> Result<RoutingStrategy, E> {
-        match s.as_ref() {
+    fn visit_str<E: Error>(&mut self, s: &str) -> Result<RoutingStrategy, E> {
+        match s {
             "Random" => Ok(RoutingStrategy::Random),
             "RoundRobin" => Ok(RoutingStrategy::RoundRobin(0)),
             _ => Ok(RoutingStrategy::default())
@@ -67,5 +67,45 @@ impl Visitor for RoutingStrategyVisitor {
 impl Default for RoutingStrategy {
     fn default() -> Self {
         RoutingStrategy::RoundRobin(0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+
+    #[cfg(feature = "rustc-serialization")]
+    use rustc_serialize::json;
+
+    #[cfg(feature = "serde-serialization")]
+    extern crate serde_json;
+
+    #[test]
+    fn test_default() {
+        assert_eq!(RoutingStrategy::default(), RoutingStrategy::RoundRobin(0));
+    }
+
+    #[test]
+    #[cfg(feature = "rustc-serialization")]
+    fn test_rustc_serialization() {
+        let routing_strategy = RoutingStrategy::Random;
+        assert_eq!(json::encode(&routing_strategy).unwrap(), "\"Random\"");
+        assert_eq!(json::decode::<RoutingStrategy>("\"Random\"").unwrap(), routing_strategy);
+
+        let routing_strategy = RoutingStrategy::RoundRobin(0);
+        assert_eq!(json::encode(&routing_strategy).unwrap(), "\"RoundRobin\"");
+        assert_eq!(json::decode::<RoutingStrategy>("\"RoundRobin\"").unwrap(), routing_strategy);
+    }
+
+    #[test]
+    #[cfg(feature = "serde-serialization")]
+    fn test_serde_serialization() {
+        let routing_strategy = RoutingStrategy::Random;
+        assert_eq!(serde_json::to_string(&routing_strategy).unwrap(), "\"Random\"");
+        assert_eq!(serde_json::from_str::<RoutingStrategy>("\"Random\"").unwrap(), routing_strategy);
+
+        let routing_strategy = RoutingStrategy::RoundRobin(0);
+        assert_eq!(serde_json::to_string(&routing_strategy).unwrap(), "\"RoundRobin\"");
+        assert_eq!(serde_json::from_str::<RoutingStrategy>("\"RoundRobin\"").unwrap(), routing_strategy);
     }
 }

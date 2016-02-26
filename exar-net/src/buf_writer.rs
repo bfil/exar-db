6f17a0917003deeb80f40e9825a1255538cff243
Bruno Filippone <bruno.filippone@b-fil.com>
2016-02-26 16:29:15 +0000

    Added tests for buf_writer

--- exar-net/src/buf_writer.rs
+++ exar-net/src/buf_writer.rs
@@ -25,3 +25,37 @@ impl<T: Write> BufWriter<T> {
 
     pub fn into_inner(self) -> T { self.inner }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+
+    use std::fs::*;
+    use std::io::{BufRead, BufReader, Write, Seek, SeekFrom};
+
+    #[test]
+    fn test_write_line() {
+        let file = OpenOptions::new().read(true).write(true).create(true)
+                                     .open("buf-writer.log").expect("Unable to create file");
+
+        let mut buf_writer = BufWriter::new(file);
+        assert!(buf_writer.write_line("line 1").is_ok());
+        assert!(buf_writer.write_line("line 2").is_ok());
+
+        let mut file = buf_writer.into_inner();
+        file.seek(SeekFrom::Start(0)).expect("Unable to seek to start");
+        let mut lines = BufReader::new(file).lines();
+
+        let line = lines.next().expect("Unable to read next line")
+                               .expect("Unable to read next line");
+
+        assert_eq!(line, "line 1".to_owned());
+
+        let line = lines.next().expect("Unable to read next line")
+                               .expect("Unable to read next line");
+
+        assert_eq!(line, "line 2".to_owned());
+
+        assert!(remove_file("buf-writer.log").is_ok());
+    }
+}
